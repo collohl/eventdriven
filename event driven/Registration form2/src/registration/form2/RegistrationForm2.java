e2e9907b8ab958409e5e8f5d65f7485cb085b094
collohl <141036449+collohl@users.noreply.github.com>
2023-12-06 16:54:10 +0300

    Add files via upload

---
+++ event driven/Registration form2/src/registration/form2/RegistrationForm2.java
@@ -0,0 +1,13 @@
+
+package registration.form2;
+
+public class RegistrationForm2 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
